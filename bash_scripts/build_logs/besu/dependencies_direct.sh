79eb8bdf400594c8083367014518a7e3acc32e00
nadia <nadia.gonzalez@bbconsult.co.uk>
2025-02-24 19:31:48 -0500

    Teku dependency analysis added

--- bash_scripts/build_logs/besu/dependencies_direct.sh
+++ bash_scripts/build_logs/besu/dependencies_direct.sh
@@ -1,35 +1,43 @@
-##### Count direct dependencies, see https://docs.gradle.org/current/userguide/java_plugin.html#sec:java_plugin_and_dependency_management
-cd ../../../nodes_src/besu-25.1.0
+grep -E '^\|    \+--- ' besu-25.1.0_dependencies_tree.txt |
+sed -E 's/^\|    \+--- ([^:]+:[^:]+):.*/\1/' |
+sort -u > besu-25.1.0_dependencies_direct.txt # sorts and removes duplicates
 
-find . -name "build.gradle" -exec cat {} \; |
-grep "implementation \|api \|runtimeOnly\|compileOnly\|testCompileOnly\|testImplementation\|testRuntimeOnly\|compileOnlyApi" |
-grep -v "project(" | tr -s ' ' |
-sort -u > besu-25.1.0_dependencies_direct.txt;
-
-echo -e "Direct dependencies: $(cat besu-25.1.0_dependencies_direct.txt | wc -l)";
-mv besu-25.1.0_dependencies_direct.txt ../../bash_scripts/build_logs/besu
-cd ../../bash_scripts/build_logs/besu
+echo -e "Direct dependencies 25.1.0: $(cat besu-25.1.0_dependencies_direct.txt | wc -l)";
 
 
-# Version 21.10.6
-cd ../../../nodes_src/besu-21.10.6
 
-find . -name "build.gradle" -exec cat {} \; |
-grep "implementation \|api \|runtimeOnly\|compileOnly\|testCompileOnly\|testImplementation\|testRuntimeOnly\|compileOnlyApi" |
-grep -v "project(" | tr -s ' ' |
-sort -u > besu-21.10.6_dependencies_direct.txt;
-echo -e "Direct dependencies: $(cat besu-21.10.6_dependencies_direct.txt | wc -l)";
-mv besu-21.10.6_dependencies_direct.txt ../../bash_scripts/build_logs/besu
-cd ../../bash_scripts/build_logs/besu
-
-
-# Version 20.10.4
-cd ../../../nodes_src/besu-20.10.4
-
-find . -name "build.gradle" -exec cat {} \; |
-grep "implementation \|api \|runtimeOnly\|compileOnly\|testCompileOnly\|testImplementation\|testRuntimeOnly\|compileOnlyApi" |
-grep -v "project(" | tr -s ' ' |
-sort -u > besu-20.10.4_dependencies_direct.txt;
-echo -e "Direct dependencies: $(cat besu-20.10.4_dependencies_direct.txt | wc -l)";
-mv besu-20.10.4_dependencies_direct.txt ../../bash_scripts/build_logs/besu
-cd ../../bash_scripts/build_logs/besu
+###### Count direct dependencies, see https://docs.gradle.org/current/userguide/java_plugin.html#sec:java_plugin_and_dependency_management
+#cd ../../../nodes_src/besu-25.1.0
+#
+#find . -name "build.gradle" -exec cat {} \; |
+#grep "implementation \|api \|runtimeOnly\|compileOnly\|testCompileOnly\|testImplementation\|testRuntimeOnly\|compileOnlyApi" |
+#grep -v "project(" | tr -s ' ' |
+#sort -u > besu-25.1.0_dependencies_direct.txt;
+#
+#echo -e "Direct dependencies: $(cat besu-25.1.0_dependencies_direct.txt | wc -l)";
+#mv besu-25.1.0_dependencies_direct.txt ../../bash_scripts/build_logs/besu
+#cd ../../bash_scripts/build_logs/besu
+#
+#
+## Version 21.10.6
+#cd ../../../nodes_src/besu-21.10.6
+#
+#find . -name "build.gradle" -exec cat {} \; |
+#grep "implementation \|api \|runtimeOnly\|compileOnly\|testCompileOnly\|testImplementation\|testRuntimeOnly\|compileOnlyApi" |
+#grep -v "project(" | tr -s ' ' |
+#sort -u > besu-21.10.6_dependencies_direct.txt;
+#echo -e "Direct dependencies: $(cat besu-21.10.6_dependencies_direct.txt | wc -l)";
+#mv besu-21.10.6_dependencies_direct.txt ../../bash_scripts/build_logs/besu
+#cd ../../bash_scripts/build_logs/besu
+#
+#
+## Version 20.10.4
+#cd ../../../nodes_src/besu-20.10.4
+#
+#find . -name "build.gradle" -exec cat {} \; |
+#grep "implementation \|api \|runtimeOnly\|compileOnly\|testCompileOnly\|testImplementation\|testRuntimeOnly\|compileOnlyApi" |
+#grep -v "project(" | tr -s ' ' |
+#sort -u > besu-20.10.4_dependencies_direct.txt;
+#echo -e "Direct dependencies: $(cat besu-20.10.4_dependencies_direct.txt | wc -l)";
+#mv besu-20.10.4_dependencies_direct.txt ../../bash_scripts/build_logs/besu
+#cd ../../bash_scripts/build_logs/besu
--- bash_scripts/build_logs/teku/dependencies_all_gav.sh
+++ bash_scripts/build_logs/teku/dependencies_all_gav.sh
@@ -2,7 +2,7 @@
 
 
 # Version 25.1.0
-grep "\---" besu-25.1.0_dependencies_tree.txt |
+grep "\---" teku-25.1.0_dependencies_tree.txt |
 grep -v "project " |
 cut -d "-" -f 4-7 |
 grep -v "(*)" |
@@ -11,12 +11,12 @@ grep -v "(*)" |
 sed -E 's/(.*):[0-9]+\.[0-9]+[^ ]* -> ([0-9]+\.[0-9]+[^ ]*)/\1:\2/' | # removes detailed version numbers (keeps version on the right of the arrow, the highest version)
 sed 's/[[:blank:]]*$//' |
 grep -v "\---" |
-sort -u > besu-25.1.0_dependencies_all_gav.txt
+sort -u > teku-25.1.0_dependencies_all_gav.txt
 
-echo -e "All dependencies (with versions) 25.1.0: $(cat besu-25.1.0_dependencies_all_gav.txt | wc -l)";
+echo -e "All dependencies (with versions) 25.1.0: $(cat teku-25.1.0_dependencies_all_gav.txt | wc -l)";
 
 # Version 24.1.0
-grep "\---" besu-24.1.0_dependencies_tree.txt |
+grep "\---" teku-24.1.0_dependencies_tree.txt |
 grep -v "project " |
 cut -d "-" -f 4-7 |
 grep -v "(*)" |
@@ -25,12 +25,12 @@ grep -v "(*)" |
 sed -E 's/(.*):[0-9]+\.[0-9]+[^ ]* -> ([0-9]+\.[0-9]+[^ ]*)/\1:\2/' | # removes detailed version numbers (keeps version on the right of the arrow, the highest version)
 sed 's/[[:blank:]]*$//' |
 grep -v "\---" |
-sort -u > besu-24.1.0_dependencies_all_gav.txt
+sort -u > teku-24.1.0_dependencies_all_gav.txt
 
-echo -e "All dependencies (with versions) 24.1.0: $(cat besu-24.1.0_dependencies_all_gav.txt | wc -l)";
+echo -e "All dependencies (with versions) 24.1.0: $(cat teku-24.1.0_dependencies_all_gav.txt | wc -l)";
 
 # Version 23.1.0
-grep "\---" besu-23.1.0_dependencies_tree.txt |
+grep "\---" teku-23.1.0_dependencies_tree.txt |
 grep -v "project " |
 cut -d "-" -f 4-7 |
 grep -v "(*)" |
@@ -39,13 +39,13 @@ grep -v "(*)" |
 sed -E 's/(.*):[0-9]+\.[0-9]+[^ ]* -> ([0-9]+\.[0-9]+[^ ]*)/\1:\2/' | # removes detailed version numbers (keeps version on the right of the arrow, the highest version)
 sed 's/[[:blank:]]*$//' |
 grep -v "\---" |
-sort -u > besu-23.1.0_dependencies_all_gav.txt
+sort -u > teku-23.1.0_dependencies_all_gav.txt
 
-echo -e "All dependencies (with versions) 23.1.0: $(cat besu-23.1.0_dependencies_all_gav.txt | wc -l)";
+echo -e "All dependencies (with versions) 23.1.0: $(cat teku-23.1.0_dependencies_all_gav.txt | wc -l)";
 
 
 # Version 22.1.0
-grep "\---" besu-22.1.0_dependencies_tree.txt |
+grep "\---" teku-22.1.0_dependencies_tree.txt |
 grep -v "project " |
 cut -d "-" -f 4-7 |
 grep -v "(*)" |
@@ -54,12 +54,12 @@ grep -v "(*)" |
 sed -E 's/(.*):[0-9]+\.[0-9]+[^ ]* -> ([0-9]+\.[0-9]+[^ ]*)/\1:\2/' | # removes detailed version numbers (keeps version on the right of the arrow, the highest version)
 sed 's/[[:blank:]]*$//' |
 grep -v "\---" |
-sort -u > besu-22.1.0_dependencies_all_gav.txt
+sort -u > teku-22.1.0_dependencies_all_gav.txt
 
-echo -e "All dependencies (with versions) 22.1.0: $(cat besu-22.1.0_dependencies_all_gav.txt | wc -l)";
+echo -e "All dependencies (with versions) 22.1.0: $(cat teku-22.1.0_dependencies_all_gav.txt | wc -l)";
 
 # Version 21.10.6
-grep "\---" besu-21.10.6_dependencies_tree.txt |
+grep "\---" teku-21.10.6_dependencies_tree.txt |
 grep -v "project " |
 cut -d "-" -f 4-7 |
 grep -v "(*)" |
@@ -68,14 +68,14 @@ grep -v "(*)" |
 sed -E 's/(.*):[0-9]+\.[0-9]+[^ ]* -> ([0-9]+\.[0-9]+[^ ]*)/\1:\2/' | # removes detailed version numbers (keeps version on the right of the arrow, the highest version)
 sed 's/[[:blank:]]*$//' |
 grep -v "\---" |
-sort -u > besu-21.10.6_dependencies_all_gav.txt
+sort -u > teku-21.10.6_dependencies_all_gav.txt
 
-echo -e "All dependencies (with versions) 21.10.6: $(cat besu-21.10.6_dependencies_all_gav.txt | wc -l)";
+echo -e "All dependencies (with versions) 21.10.6: $(cat teku-21.10.6_dependencies_all_gav.txt | wc -l)";
 
 # ------------------------------------------------------------------------------------------------
 #
-## Dependency tree besu-20.10.4
-#grep "\---" besu-20.10.4_dependencies_tree.txt |
+## Dependency tree teku-20.10.4
+#grep "\---" teku-20.10.4_dependencies_tree.txt |
 #grep -v "project " |
 #cut -d "-" -f 4-7 |
 #grep -v "(*)" |
@@ -83,6 +83,6 @@ echo -e "All dependencies (with versions) 21.10.6: $(cat besu-21.10.6_dependenci
 ##sed -E 's/:[0-9]+\.[0-9]+.*$/ /g' |
 #sed 's/[[:blank:]]*$//' |
 #grep -v "\---" |
-#sort -u > besu-20.10.4_dependencies_all_gav.txt
+#sort -u > teku-20.10.4_dependencies_all_gav.txt
 #
-#echo -e "All dependencies: $(cat besu-20.10.4_dependencies_all_gav.txt | wc -l)";
+#echo -e "All dependencies: $(cat teku-20.10.4_dependencies_all_gav.txt | wc -l)";
